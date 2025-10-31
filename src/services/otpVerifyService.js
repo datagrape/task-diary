36c5c3dbae469cbd939f126e043ae1bc7db90ebf
ashish031990 <ashish.sharma031990@gmail.com>
2025-10-31 23:19:29 +0530

    updated otpverify logic again

--- src/services/otpVerifyService.js
+++ src/services/otpVerifyService.js
@@ -1,14 +1,16 @@
 const { Prisma } = require('@prisma/client');
 const prisma = require('../prismaClient');
 
+
 // Verify OTP from DB
 exports.verifyOtp = async (email) => {
-    return prisma.user.findUnique({
-        where: {
-            email: {
-                equals: email,
-                mode: 'insensitive',
-            },
-        },
+    const user = await prisma.user.findFirst({
+        where: { email: { equals: email, mode: 'insensitive' } },
     });
+
+    if (!user) {
+        throw new Error('User with that email not found');
+    }
+    return prisma.user.findUnique({ where: { id: user.id } });
 };
+
