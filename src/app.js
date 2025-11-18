e2e316be60fc4ce7cbb64f619fa251a1748bf129
ashish031990 <ashish.sharma031990@gmail.com>
2025-11-18 17:55:03 +0530

    changed red to blue in button changed page path

--- src/app.js
+++ src/app.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 /* ---------------- Serve HTML/static files ---------------- */
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* Optional pretty route: /otp */
-app.get('/otp', (req, res) => {
+/* Optional pretty route: /account-delete */
+app.get('/account-delete', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'otp.html'));
 });
 
